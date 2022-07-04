d934bb1d5c3d8e47442a19141bb2e3ea5fdcc3af
naymur92 <naymurrahman@yahoo.com>
2022-07-04 18:49:03 +0600

    04-06

--- PHP/04Jun2022/superGlobals.php
+++ PHP/04Jun2022/superGlobals.php
@@ -1,9 +1,24 @@
+<h1>$_SERVER superGlobal</h1>
 <?php
     echo "<pre>";
     print_r($_SERVER);
 
     echo "<br>";
-
     echo $_SERVER['PHP_SELF'];
-    echo "</pre>";
+
+    echo "<br>";
+    echo $_SERVER['REMOTE_ADDR'];
+
+    echo "<br>";
+    echo $_SERVER['REQUEST_URI'];
+
+    echo "<br>";
+    echo $_SERVER['HTTP_USER_AGENT'];
+    // echo "</pre>";
+?>
+
+<h1>$_GET superGlobal</h1>
+<a href="superGlobals.php?name=Robbany">Click Here</a>
+<?php
+    print_r($_GET);
 ?>
