d249bb55894b251df84289a1c9489c9b28fb1c76
naymur92 <naymurrahman@yahoo.com>
2022-07-21 08:36:32 +0600

    21-06-home

--- PHP/20Jul2022/basic.php
+++ PHP/20Jul2022/basic.php
@@ -1,20 +1,20 @@
 <?php
-    echo "Hello World again!";
+    echo "Hello World again!\n";
 
     //variable
-    $abc = "Today is Wednesday.";
-    echo "<br>";
+    $abc = "Today is Wednesday.\n";
+    // echo "<br>";
     print $abc;
 
     $number = 100;
     $text = "Some Text";
     $fruits = Array("Mango", "Banana", "Apple", "Pulm");
 
-    echo "<br><pre>";
-    print_r($fruits);
-    echo "<br>";
+    // echo "<br><pre>";
+    print_r($fruits)."\n";
+    // echo "<br>";
 
     //SuperGlobals
     print_r($_SERVER);
-    echo "<br>";
+    // echo "<br>";
 ?>
--- PHP/20Jul2022/escape_sequence.php
+++ PHP/20Jul2022/escape_sequence.php
@@ -3,7 +3,7 @@
     $output1 = 'This is one line. \n And this is another line. <br>';
     $output2 = 'This is one line.' . "\n" . 'And this is another line';
 
-    echo "<pre>";
+    // echo "<pre>";
     echo $output;
     echo $output1;
     echo $output2;
