1485ee9bb5ee2e6df58055605a91c927a9c1f41d
naymur92 <naymurrahman@yahoo.com>
2022-07-24 13:22:04 +0600

    24-07-home

--- PHP/23Jul2022/anonymous.php
+++ PHP/23Jul2022/anonymous.php
@@ -1,10 +1,19 @@
 <?php
     $x = 15;
-    $output = function(){
-        global $x;
+    // $output = function(){
+    $output = function() use($x){
+    // $output = function() use(&$x){
         $x += 100;
         echo $x . "\n";
     };
     $output();
     echo $x ."\n";
+?>
+
+<?php
+    // function myEcho(){
+    //     echo "Clouser.\n";
+    // };
+    // $output = "myEcho";
+    // $output();
 ?>
