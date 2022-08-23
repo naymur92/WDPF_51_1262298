36aab08591039bc72d982942d5e7aa28ff96ae34
naymur92 <naymurrahman@yahoo.com>
2022-08-23 18:33:49 +0600

    23-08

---
+++ PHP&MySQL/23Aug2022/checkUser.php
@@ -0,0 +1,19 @@
+<?php
+    include_once('dbconfig.php');
+
+    $email = $_POST['email'];
+    $pass = sha1($_POST['password']);
+
+    $sql = "SELECT * FROM users WHERE email='$email' AND password='$pass'";
+
+    $result = $db->query($sql);
+    if($result->num_rows != 1){
+        $msg = "Email or Password may be wrong";
+        session_start();
+        header("Location:index.php?m=$msg");
+    } else{
+        session_start();
+        $_SESSION['email'] = $email;
+        header("Location:dashboard.php");
+    }
+?>
---
+++ PHP&MySQL/23Aug2022/dbconfig.php
@@ -0,0 +1,7 @@
+<?php
+    define("HOST", "localhost");
+    define("USER", "root");
+    define("PASS", "");
+    define("DB", "wdpf51");
+    $db = new mysqli(HOST, USER, PASS, DB);
+?>
