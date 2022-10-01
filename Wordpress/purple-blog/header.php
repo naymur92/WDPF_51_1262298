b3b8f628b47164ec47a2f18a83b96c9e1f00cae5
naymur92 <naymurrahman@yahoo.com>
2022-10-01 14:47:24 +0600

    01-10-class

---
+++ Wordpress/purple-blog/header.php
@@ -0,0 +1,25 @@
+<div id="logo">
+    <h1><a href="<?=home_url(); ?>">Honey Link</a></h1>
+    <h2 id="tagline">Committed to quality</h2>
+  </div>
+  <div id="menu"> <a href="#" id="login">login</a> <a href="#" id="rss-entries">rss entries</a> <a href="#" id="rss-comments">rss comments</a> </div>
+  <div class="clearing">&nbsp;</div>
+  <ul id="nav">
+    <li><a href="<?=home_url(); ?>">&nbsp;&nbsp;Home&nbsp;</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Services</a></li>
+    <li><a href="#">Portfolio</a></li>
+    <li><a href="#">Contact</a></li>
+    <li><a href="#">Henry</a></li>
+  </ul>
+  <div id="search">
+    <form method="get" id="searchform" action="#">
+      <div id="s-text">
+        <input value="Search" name="s" id="s" type="text" />
+        <label for="s">Search</label>
+      </div>
+      <div id="s-submit">
+        <input id="searchsubmit" value="" type="submit" />
+      </div>
+    </form>
+  </div>
---
+++ Wordpress/purple-blog/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title></title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="<?= get_stylesheet_uri() ?>" type="text/css" media="screen" />
+</head>
+<body>
+<div id="container">
+  <?php get_header() ?>
+  <div id="posts">
+    <?php
+      if(have_posts()):
+        while(have_posts()):
+          the_post();
+    ?>
+    <div class="post">
+      <h2><a href="<?=the_permalink(); ?>"><?= the_title(); ?> (<?= the_id(); ?>)</a></h2>
+      <p class="date"><?=the_time('F j, Y - H:s A'); ?></p>
+      <div class="entry">
+        <p class="thumb"><a href="<?=the_permalink(); ?>"><?=the_post_thumbnail(); ?></a></p>
+        <p><?php //the_content('Read more', false); ?></p>
+        <p><?php the_excerpt(); ?></p>
+      </div>
+      <div class="postmetadata">
+        <span class="tags"><?=the_category(); ?></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span class="comments-no">
+        <?php comments_number( 'no responses', 'one response', '% responses' ); ?>
+          <!-- <a href="#">1 Comment</a> -->
+        </span> <br />
+      </div>
+    </div>
+    <?php          
+        endwhile;
+      endif;
+    ?>
+  </div>
+<?php get_sidebar() ?>
+</div>
+<?php get_footer() ?>
+</body>
+</html>
