7e3e548786a0cbb574bd115bd82ac1404bddc55d
naymur92 <naymurrahman@yahoo.com>
2022-10-14 10:58:43 +0600

    14-10-home

---
+++ Wordpress/epora/functions.php
@@ -0,0 +1,10 @@
+<?php
+  add_theme_support('post-thumbnails');
+
+  function epora_register_nav_menu(){
+		register_nav_menus( array(
+	    	'primary_menu' => __( 'Primary Menu', 'text_domain' ),
+	    	'footer_menu'  => __( 'Footer Menu', 'text_domain' ),
+		) );
+	}
+	add_action( 'after_setup_theme', 'epora_register_nav_menu', 0 );
